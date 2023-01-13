4b41684616b745306329a04301c6c3d13a9a9187
sprakhar11 <prakhar.infinity@gmail.com>
2023-01-13 19:44:29 +0530

    impleted Dijkstra

--- C_Dijkstra.cpp
+++ C_Dijkstra.cpp
@@ -1,119 +1,181 @@
+
+
+// dheere dheere re mana, dheere sab kuch hoye
+// mali seenche so ghara, ritu aaye phal hoye 
+
 #include <bits/stdc++.h>
+#include <unordered_set>
+#include <chrono>
+#define ll long long 
+#define int long long 
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"   
+#define vi vector<int>
+#define pb push_back
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define f(i,a,b) for(ll i=a;i<b;i++)
+	
 using namespace std;
+#define watch(x) cout << (#x) << " = " << (x) << endl
+const int MOD = 1e9 + 7;
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll mult(ll x, ll y) {ll res = x * y;return (res >= MOD ? res % MOD : res);}
+ll factorial(ll v){ll ans=1;for(int i=2;i<=v;i++){ans*=i;ans%=MOD;}return ans;}
+ll power(ll x, ll y){   if (y < 0)return 1;ll res = 1; x %= MOD;while (y!=0) {if ((y & 1)==1)res = mult(res, x); y >>= 1;x = mult(x, x);} return res;}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+void vin( vector<int> &v , int n ){for (int i = 0; i < n; i++){int x ;cin >> x; v.push_back(x);}}
+void vout(vector<int> &v){for (int i = 0; i < v.size(); i++){cout << v[i] << " " ;}cout << endl;}
+ll highestPowerof2(ll n){ ll p = (ll)log2(n);   return (ll)pow(2, p);}
+bool isPowerOfTwo(int n){ if(n==0)   return false;   return (ceil(log2(n)) == floor(log2(n)));}
+bool chkprime(int n){ for(int i = 2 ; i*i <= n ; i++){ if( n % i == 0) return false;} return true; }
+string to_binary(int n) { string s = ""; for (int i = 31; i >= 0; i--) { int k = n >> i; if (k & 1) s = s + "1";else s = s + "0";}return s ; }
+static void removeTrailingCharacters(std::string &str, const char charToRemove) {str.erase (str.find_last_not_of(charToRemove) + 1, std::string::npos );}
+static void removeLeadingCharacters(std::string &str, const char charToRemove) {str.erase(0, std::min(str.find_first_not_of(charToRemove), str.size() - 1));}
+long long lcm(int a, int b){    return (a / gcd(a, b)) * b;}
+int digits_count(int n){int d=0;while(n != 0){d++;n /=10;}return d;}
 
-int NO_PARENT = -1;
-void printPath(int currentVertex, vector<int> parents)
-{
+void prakhar() { 
 
-	if (currentVertex == NO_PARENT) {
+	int v , e ;
+	cin >> v >> e ;
+	//considering O indexing
+	vector<vector<int>> adj(v, vector<int>());
+	vector<vector<int>> weight(v, vector<int>(v, 0));
+	
+	for (int i = 0; i < e; i++){
+		int u, s, w;
+		cin >> u >> s >> w;
+		u--;
+		s--;
+
+		adj[u].pb(s);
+		adj[s].pb(u);
+
+		weight[u][s] = w;
+		weight[s][u] = w;
+	}
+	//for debugging
+	// for(auto it:weight){
+	// 	for(auto it2:it){
+	// 		cout << it2 << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	vector<int> dist(v, INT_MAX);
+	dist[0] = 0;
+	map<int,int> parent; // stores parent of updated weight
+	priority_queue<int, vector<int>, greater<int>> pq; //stores minnimum weight
+	map<int, int> weight_index; // mapping of minimum weight and index
+	vector<bool> marked(v, 0);
+
+	weight_index[0] = 0;
+	pq.push(0);
+	
+	for (int i = 0; i < v; i++) {
+		//minimum vertex which is unmarked
+		int min_vertex;
+		if(!pq.empty()) {
+			min_vertex = weight_index[pq.top()];
+			// cout << min_vertex << endl;
+			marked[min_vertex] = true;
+			pq.pop();
+		}
+
+		// cout << "hit1 ";
+
+
+		for(auto ch_vertex:adj[min_vertex]) {
+
+			// cout << "hit2";
+
+			
+			if(marked[ch_vertex] == false) {
+				// cout << "hit3 " << endl;
+
+				// cout << ch_vertex << " " << dist[ch_vertex] << endl;
+
+				if(dist[ch_vertex] > (dist[min_vertex] + weight[min_vertex][ch_vertex])) {
+					//update parent of ch_vertex
+					parent[ch_vertex] = min_vertex;
+					dist[ch_vertex] = dist[min_vertex] + weight[min_vertex][ch_vertex];
+					pq.push(dist[ch_vertex]);
+					weight_index[dist[ch_vertex]] = ch_vertex;
+				}
+			}
+
+		}
+		// for(auto it:dist)
+		// {
+		// 	cout << it << " ";
+		// }
+		// cout << endl;
+
+	}
+
+	// for(auto it:dist)
+	// {
+	// 	cout << it << " " << endl;
+	// }
+	if(dist[v-1] == INT_MAX) {
+		cout << -1 << endl;
 		return;
 	}
-    cout << "Hit 2";
 
-	printPath(parents[currentVertex], parents);
-	cout << currentVertex + 1 << " ";
-}
-void printSolution(int startVertex, vector<int> distances,
-				vector<int> parents)
-{
-	int nVertices = distances.size();
-    cout << "Hit 1";
-    printPath(nVertices - 1, parents);
-}
-
-
-void dijkstra(vector<vector<int> > adjacencyMatrix,
-			int startVertex)
-{
-    cout << " sdfsdd2323f";
-
-	int nVertices = adjacencyMatrix[0].size();
-
-	vector<int> shortestDistances(nVertices);
-	vector<bool> added(nVertices);
-	for (int vertexIndex = 0; vertexIndex < nVertices;
-		vertexIndex++) {
-		shortestDistances[vertexIndex] = INT_MAX;
-		added[vertexIndex] = false;
+	int k = v - 1;
+	vi ans;
+	ans.pb(k);
+	while(parent[k] != 0)
+	{
+		ans.pb(parent[k]);
+		k  = parent[k];
 	}
-	shortestDistances[startVertex] = 0;
 
-	vector<int> parents(nVertices);
+	ans.pb(0);
 
-	parents[startVertex] = NO_PARENT;
-
-	for (int i = 1; i < nVertices; i++) {
-
-		int nearestVertex = -1;
-		int shortestDistance = INT_MAX;
-		for (int vertexIndex = 0; vertexIndex < nVertices;
-			vertexIndex++) {
-			if (!added[vertexIndex]
-				&& shortestDistances[vertexIndex]
-					< shortestDistance) {
-				nearestVertex = vertexIndex;
-				shortestDistance
-					= shortestDistances[vertexIndex];
-			}
-		}
-
-		added[nearestVertex] = true;
-
-		for (int vertexIndex = 0; vertexIndex < nVertices;
-			vertexIndex++) {
-			int edgeDistance
-				= adjacencyMatrix[nearestVertex]
-								[vertexIndex];
-
-			if (edgeDistance > 0
-				&& ((shortestDistance + edgeDistance)
-					< shortestDistances[vertexIndex])) {
-				parents[vertexIndex] = nearestVertex;
-				shortestDistances[vertexIndex]
-					= shortestDistance + edgeDistance;
-			}
-		}
+	for(int i = ans.size() - 1 ; i >= 0 ; i--)
+	{
+		cout << ans[i] + 1 << " ";
 	}
-    cout << " sdfsdf";
-	printSolution(startVertex, shortestDistances, parents);
+
+	   
 }
 
-int main()
-{
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
-    #endif
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+	#endif
+	auto start = chrono::steady_clock::now();
 
-    int V, e;
-    cin >> V >> e;
-    // cout << " sdfsdf";
-
-    
-	vector<vector<int>> adjacencyMatrix(V, vector<int>(V, 0));
-
-    for (int i = 0; i < e; i++)
-    {
-        int u , v , w ;
-        cin >> u >> v >> w;
-        u--;
-        v--;
-
-    	adjacencyMatrix[u][v] = w;
-    	adjacencyMatrix[v][u] = w;
-        cout << " sdfsdf";
+	//  Insert the code that will be timed
 
 
+	ll t = 1 ;
+	// cin >> t ;
+	int ii;
+	for ( ii = 1; ii <= t; ii++) {
+		//  cout << "Case #" << i <<": ";
+		prakhar();
+	}
 
+	auto end = chrono::steady_clock::now();
 
-    }
-
-    for(auto it:adjacencyMatrix)
-    cout << " sdfsdf";
-
-	dijkstra(adjacencyMatrix, 0);
-    cout << " sdfsdf";
-
+	auto diff = end - start;
+	cerr << chrono::duration <double, nano> (diff).count() << " ns" << endl;
+	
 	return 0;
+	
 }
