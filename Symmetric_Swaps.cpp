80cd599c7a8158030a8f5b6755969919765ee28e
sprakhar11 <prakhar.infinity@gmail.com>
2023-01-15 10:42:11 +0530

    lond_challenge

---
+++ Symmetric_Swaps.cpp
@@ -0,0 +1,310 @@
+
+
+// dheere dheere re mana, dheere sab kuch hoye
+// mali seenche so ghara, ritu aaye phal hoye 
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <chrono>
+#define ll long long 
+#define int long long 
+#define MOD1 998244353
+#define INF 1e18
+#define endl "\n"   
+#define vi vector<int>
+#define pb push_back
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define ff first
+#define ss second
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define f(i,a,b) for(ll i=a;i<b;i++)
+    
+using namespace std;
+#define watch(x) cout << (#x) << " = " << (x) << endl
+const int MOD = 1e9 + 7;
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll mult(ll x, ll y) {ll res = x * y;return (res >= MOD ? res % MOD : res);}
+ll factorial(ll v){ll ans=1;for(int i=2;i<=v;i++){ans*=i;ans%=MOD;}return ans;}
+ll power(ll x, ll y){   if (y < 0)return 1;ll res = 1; x %= MOD;while (y!=0) {if ((y & 1)==1)res = mult(res, x); y >>= 1;x = mult(x, x);} return res;}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+void vin( vector<int> &v , int n ){for (int i = 0; i < n; i++){int x ;cin >> x; v.push_back(x);}}
+void vout(vector<int> &v){for (int i = 0; i < v.size(); i++){cout << v[i] << " " ;}cout << endl;}
+ll highestPowerof2(ll n){ ll p = (ll)log2(n);   return (ll)pow(2, p);}
+bool isPowerOfTwo(int n){ if(n==0)   return false;   return (ceil(log2(n)) == floor(log2(n)));}
+bool chkprime(int n){ for(int i = 2 ; i*i <= n ; i++){ if( n % i == 0) return false;} return true; }
+string to_binary(int n) { string s = ""; for (int i = 31; i >= 0; i--) { int k = n >> i; if (k & 1) s = s + "1";else s = s + "0";}return s ; }
+static void removeTrailingCharacters(std::string &str, const char charToRemove) {str.erase (str.find_last_not_of(charToRemove) + 1, std::string::npos );}
+static void removeLeadingCharacters(std::string &str, const char charToRemove) {str.erase(0, std::min(str.find_first_not_of(charToRemove), str.size() - 1));}
+long long lcm(int a, int b){    return (a / gcd(a, b)) * b;}
+int digits_count(int n){int d=0;while(n != 0){d++;n /=10;}return d;}
+
+void swap(vector<pair<int, int>> &p, int i ,int j){
+    int tmp = p[i].ff;
+    p[i].ff = p[j].ss;
+    p[j].ss = tmp;
+}
+
+bool sort_p(pair<int, int> &a, pair<int, int> &b) {
+    return a.ff < b.ff;
+}
+
+
+int right_check(vector<pair<int, int>> &a,int n) {
+
+    int t_ans = a[n-1].ff - a[0].ff;
+    if(a[n-1].ss < a[n-1].ff && a[n-1].ss >= a[0].ff) {
+        swap(a, n-1, n-1);
+        t_ans = min(t_ans , a[n-1].ff - a[0].ff);
+    }
+    return t_ans;
+
+    /*
+    vi tmp_a = a;
+    int f1 = 0;
+
+    if(b[n-1] < a[n-1] && b[n-1] >= a[0]) {
+        swap(a, b, n-1, n-1);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    }
+
+
+    sort(all(a));
+
+    if(b[0] > a[0] && b[0] <= a[n-1]) {
+        swap(a, b, 0 , 0);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    } else {
+        swap(a, b, 0 , 0);
+    }
+
+    
+    sort(all(a));
+    swap(a, b, n-1, n-1);
+    sort(all(a));
+
+
+    if(b[0] > a[0] && b[0] <= a[n-1]) {
+        swap(a, b, 0 , 0);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    }
+
+    swap(a, b, 0 , 0);
+
+    return t_ans;
+    */
+}
+
+int left_check(vector<pair<int, int>> &a,int n) {
+
+    int t_ans = a[n-1].ff - a[0].ff;
+
+    if(a[0].ss > a[0].ff && a[0].ss <= a[n-1].ff) {
+        swap(a, 0 , 0);
+        t_ans = min(t_ans , a[n-1].ff - a[0].ff);
+    }
+    return t_ans;
+
+    /*
+    vi tmp_a = a;
+    int f1 = 0;
+
+    if(b[n-1] < a[n-1] && b[n-1] >= a[0]) {
+        swap(a, b, n-1, n-1);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    }
+
+
+    sort(all(a));
+
+    if(b[0] > a[0] && b[0] <= a[n-1]) {
+        swap(a, b, 0 , 0);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    } else {
+        swap(a, b, 0 , 0);
+    }
+
+    
+    sort(all(a));
+    swap(a, b, n-1, n-1);
+    sort(all(a));
+
+
+    if(b[0] > a[0] && b[0] <= a[n-1]) {
+        swap(a, b, 0 , 0);
+        sort(all(a));
+        t_ans = min(t_ans , a[n-1] - a[0]);
+    }
+
+    swap(a, b, 0 , 0);
+
+    return t_ans;
+    */
+}
+
+
+void prakhar() { 
+
+    int n;
+    cin >> n ;
+    vi a, b;
+    vin(a, n);
+    vin(b, n);
+
+    vector<pair<int, int>> p;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        p.pb({a[i], b[i]});
+    }
+    
+
+    vector<pair<int, int>> tmp_p = p;
+
+    int ans1 = INT_MAX; 
+
+    sort(all(p), sort_p); 
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = right_check(p, n);
+        if(k > ans1)
+        break;
+        ans1 = min(k, ans1);
+    }
+
+    p =tmp_p;
+    for (int i = 0; i < n; i++)
+    {
+        int k = left_check(p, n);
+        if(k > ans1)
+        break;
+        ans1 = min(k, ans1);
+    }
+
+    p.clear();
+    
+    for (int i = 0; i < n; i++)
+    {
+        p.pb({b[i], a[i]});
+    }
+    
+    sort(all(p), sort_p); 
+
+
+    tmp_p = p;
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = right_check(p, n);
+        if(k > ans1)
+        break;
+        ans1 = min(k, ans1);
+    }
+
+    p =tmp_p;
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = left_check(p, n);
+        if(k > ans1)
+        break;
+        ans1 = min(k, ans1);
+    }
+
+    cout << ans1 << endl;
+       
+}
+
+void prakhar() {
+    int n; cin >> n ;
+    vi a;
+    vi b;
+    vector<pair<int, int>> p;
+    vin(a, n);
+    vin(b, n);
+    for (int i = 0; i < n; i++){
+        p.pb({a[i], i});
+        p.pb({b[i], i});
+    }
+    sort(all(p), sort_p);
+    
+    // for(auto it:p) {
+    //     cout << it.ff << " " << it.ss << endl;
+    // }
+
+    int i = 0;
+    int j = -1;
+    int count = 0;
+    map<int, int> mp;
+    int ans = p[2*n - 1].ff - p[0].ff;
+
+    while (true){
+        // cout << i << " " << j << " " << count << endl;
+
+        if(count == n) {
+            int d = p[j].ff - p[i].ff;
+            ans = min(ans, d);
+
+            mp[p[i].ss]--;
+            if(mp[p[i].ss] == 0)
+                count--;
+            i++;
+        } else {
+
+            j++;
+            if(j == 2 * n )
+            break;
+
+            mp[p[j].ss]++;
+            if(mp[p[j].ss] == 1)
+                count++;
+        }
+
+        if(i > j) break;
+
+    }
+    cout << ans << endl;
+
+    
+
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+    auto start = chrono::steady_clock::now();
+
+    //  Insert the code that will be timed
+
+
+    ll t = 1 ;
+    cin >> t ;
+    int ii;
+    for ( ii = 1; ii <= t; ii++) {
+        //  cout << "Case #" << i <<": ";
+        solve();
+    }
+
+    auto end = chrono::steady_clock::now();
+
+    auto diff = end - start;
+    cerr << chrono::duration <double, nano> (diff).count() << " ns" << endl;
+    
+    return 0;
+    
+}
