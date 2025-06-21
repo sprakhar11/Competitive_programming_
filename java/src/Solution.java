28fce5f7ecc97bba3435e0c826c8be3667f354f7
Prakhar Sharma <76238592+sprakhar11@users.noreply.github.com>
2025-06-21 21:20:59 +0530

    random problem

--- java/src/Solution.java
+++ java/src/Solution.java
@@ -1,5 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -36,8 +38,35 @@ public class Solution {
             return a;
         }
 
+        String nextLine() {
+            try {
+                if (st.hasMoreTokens()) {
+                    StringBuilder sb = new StringBuilder();
+                    while (st.hasMoreTokens()) {
+                        sb.append(st.nextToken());
+                        if (st.hasMoreTokens()) sb.append(" ");
+                    }
+                    return sb.toString();
+                }
+                return br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+
         long nextLong() {
             return Long.parseLong(next());
+        }
+    }
+    static class Pair<T, V> {
+        T first;
+        V second;
+
+        public Pair(T first, V second){
+            this.first = first;
+            this.second = second;
         }
     }
     static FastScanner fs = null;
@@ -52,74 +81,113 @@ public class Solution {
         }
 
         fs = new FastScanner();
-        int t = fs.nextInt();
+        int t = 1;
+//        t = fs.nextInt();
         while(t-- > 0){
-            solve(t + 1);
+//            solve();
+            String caption = fs.nextLine();
+            System.out.println(minSwaps(new int[]{1, 2, 2}));
         }
     }
 
-    static class Pair<T, V>{
-        T first;
-        V second;
-
-        public Pair(T first, V second){
-            this.first = first;
-            this.second = second;
+    static public int minSwaps(int[] nums) {
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                even.add(i);
+            } else {
+                odd.add(i);
+            }
         }
+        if (Math.abs(even.size() - odd.size()) > 1) {
+            return -1;
+        }
+
+        // check if swap needed
+        int a = solve(even, odd);
+        int b = solve(odd, even);
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        }
+        return Math.min(a, b);
     }
+
+    static int solve(List<Integer> even, List<Integer> odd) {
+        int temp = 0;
+        int oddIndexCompleted = 0;
+        int ans = 0;
+        for (int i = 1; i < even.size(); i++) {
+            if (even.get(i) - even.get(i - 1) == 1) {
+                // put an odd number at i + temp
+                ans += Math.abs(odd.get(oddIndexCompleted++) - (i + temp));
+                temp++;
+            }
+
+            if (even.get(i) > 2) {
+                oddIndexCompleted++;
+            }
+        }
+        return ans;
+    }
+
+
+    static char toUpper(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return (char) (c - 32);
+        }
+        return c;
+    }
+
+    static char toLower(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char) (c + 32);
+        }
+        return c;
+    }
+
+    static String makeAllLowerCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(toLower(s.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    static String makeAllUpperCase(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            sb.append(toUpper(s.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    static Boolean isAlphaNumericSpace(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isLetterOrDigit(s.charAt(i)) && s.charAt(i) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    static public String generateTag(String caption) {
+        String[] tag = caption.split(" ");
+        String ans = "#" + toLower(tag[0].charAt(0)) + makeAllLowerCase(tag[0].substring(1));
+        for (int i = 1; i < tag.length ; i++) {
+            String s = toUpper(tag[i].charAt(0)) + makeAllLowerCase(tag[i].substring(1));
+            ans += s;
+        }
+
+        return ans.substring(0, Math.min(ans.length(),100) );
+    }
+
+
+
     static void solve(int t) {
-        int n =  fs.nextInt();
-        int m = fs.nextInt();
-        int[][] arr = new int[n][m];
 
-
-        int max = -1;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                arr[i][j] = fs.nextInt();
-                max = Math.max(max, arr[i][j]);
-            }
-        }
-        if ((t == 2) && (n == 4) &&( m == 25000)) {
-            int nn =  fs.nextInt();
-            int mm = fs.nextInt();
-            System.out.println(nn + " " + mm);
-            int[][] arr2 = new int[nn][mm];
-            for(int i = 0; i < nn; i++){
-                for(int j = 0; j < mm; j++){
-                    arr2[i][j] = fs.nextInt();
-                    System.out.print(arr2[i][j] + " ");
-                }
-                System.out.println();
-            }
-            return;
-        }
-
-        HashSet<Pair<Integer, Integer>> set = new HashSet<>();
-
-        for (int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if (arr[i][j] == max){
-                    set.add(new Pair(i, j));
-                }
-            }
-        }
-        HashSet<Integer> alreadyDone = new HashSet<>();
-        for (Pair<Integer, Integer> pair: set){
-            if (!alreadyDone.contains(pair.first)){
-                HashSet<Integer> set2 = new HashSet<>();
-                for (Pair<Integer, Integer> pair2: set){
-                    if(pair2.first != pair.first) {
-                        set2.add(pair2.second);
-                    }
-                }
-                if (set2.size() <= 1){
-                    System.out.println(max - 1);
-                    return;
-                }
-            }
-            alreadyDone.add(pair.first);
-        }
-        System.out.println(max);
     }
 }
